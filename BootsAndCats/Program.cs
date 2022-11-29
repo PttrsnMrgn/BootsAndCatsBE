8fba320c76f7c327b79b7f0c25cbf7eaf3f498af
nolto1sj <sjnolton@gmail.com>
2022-11-28 20:45:03 -0500

    Added routing to Get Users and connected to localdb

--- BootsAndCats/Program.cs
+++ BootsAndCats/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BootsAndCats;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BootsAndCatsContext>(options => options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+
 
 var app = builder.Build();
 
