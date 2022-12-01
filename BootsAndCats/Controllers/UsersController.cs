a5fdbb968f28f9f98686415f1e387daf5370054f
nolto1sj <sjnolton@gmail.com>
2022-12-01 18:46:14 -0500

    something

--- BootsAndCats/Controllers/UsersController.cs
+++ BootsAndCats/Controllers/UsersController.cs
@@ -51,7 +51,6 @@ namespace BootsAndCats.Controllers
         }
 
         // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutUser(int id, User user)
         {
